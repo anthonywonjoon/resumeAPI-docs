21ce3a32c60968c11dcb5536e27cea6daef5b5cf
anthonywonjoon <89366304+anthonywonjoon@users.noreply.github.com>
2023-10-12 12:26:21 -1000

    Configuration & Index update

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -20,6 +20,7 @@ const config = {
   // If you aren't using GitHub pages, you don't need these.
   organizationName: 'anthonywonjoon', // Usually your GitHub org/user name.
   projectName: 'resumeapi-docs', // Usually your repo name.
+  deploymentBranch: 'deployment',
 
   onBrokenLinks: 'throw',
   onBrokenMarkdownLinks: 'warn',
